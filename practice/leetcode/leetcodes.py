1403d37957d6babe8487fc06af9f1aa601b6d510
Sophie Slagle <Slagsoph@isu.edu>
2023-02-01 20:38:22 -0800

    practice

--- practice/leetcode/leetcodes.py
+++ practice/leetcode/leetcodes.py
@@ -44,3 +44,16 @@ class Solution(object):
 
     l1.reverse()
     print(l1)
+
+
+def transform(a = 2):
+    if a == 1:
+        return a +- 2
+    return a
+
+total = 1
+
+for x in [3,5,1]:
+    total = total + transform(x)
+
+print(total)
