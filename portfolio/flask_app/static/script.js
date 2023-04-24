325851591fd757186604f100058806b45ed797a2
Sophie Slagle <Slagsoph@isu.edu>
2023-04-24 14:45:29 -0700

    added resume page

---
+++ portfolio/flask_app/static/script.js
@@ -0,0 +1,8 @@
+const scrollToTop = document.querySelector(".toTopBtn");
+
+scrollToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
