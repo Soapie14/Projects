a4c4de246ceb515d2f2a1bd87bde3bed058481e9
Sophie Slagle <slagsoph@isu.edu>
2023-06-21 12:02:46 -0700

    cs50 notes/library

---
+++ practice/cs50/week1/hello.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// standard IO(input and output).h
+// include is like including a library 
+
+// manual pages
+// manual.cs50.io
+
+
+
+int main(void)
+{
+    printf("hello, world");
+}
+
