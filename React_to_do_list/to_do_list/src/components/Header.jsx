76060cd77ddfbbf6ac6e0e8e376237c6a79d28a5
Sophie Slagle <slagsoph@isu.edu>
2024-08-13 10:21:50 -0700

    header with font added

--- React_to_do_list/to_do_list/src/components/Header.jsx
+++ React_to_do_list/to_do_list/src/components/Header.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div className="bg-black text-green-700">
-            Hello World
+        <div className="flex items-center justify-center text-7xl mt-10 mb-10">
+            To-Do List
         </div>
+        
     );
 };
 export default Header;
