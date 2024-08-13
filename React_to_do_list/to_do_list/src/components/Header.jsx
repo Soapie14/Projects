3cbb023be6e03ec342c23d5e9a4087845d4cd5e7
Sophie Slagle <slagsoph@isu.edu>
2024-08-13 09:42:18 -0700

    initial commit

---
+++ React_to_do_list/to_do_list/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="bg-black text-green-700">
+            Hello World
+        </div>
+    );
+};
+export default Header;
